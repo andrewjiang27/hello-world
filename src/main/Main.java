8ddda98d3468ba8deb2da6ec9157e3e835cdfac5
Grace-Chen-12 <72233633+Grace-Chen-12@users.noreply.github.com>
2020-10-01 16:08:39 -0700

    Update Main.java
    Making the text better and the code more efficient.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,5 +3,7 @@ package main;
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("wow congratz on git");
+		System.out.println("Thank you so much!");
+		System.out.println(":)");
 	}
 }
